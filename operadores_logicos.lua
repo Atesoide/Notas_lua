487ac1756bd64eed464b76364d80b58b44f721c2
Atesoide <josue.naranja@gmail.com>
2025-03-18 19:55:05 -0600

    no cambié nada aquí, entonces (???)

--- operadores_logicos.lua
+++ operadores_logicos.lua
@@ -5,6 +5,10 @@
 dato1 = true
 dato2 = false
 
+var1, var2, var3 = true, true, false
+resultado = var1 and var2 or var3 -- se hace la comparación secuencialmente, se puden agregar paréntesis para cambiar la jerarquía
+
+
 print(dato1 or dato2) --retorna true
 print(dato1 and dato2) -- retorna false
 print(not dato1) --retorna false
