2811aa47f5f904e9404e74494ea8e648e5a32b97
Atesoide <josue.naranja@gmail.com>
2025-03-18 20:16:31 -0600

    [INTRODUCE] al ciclo for ipairs (el foreach de toda la vida)

--- ciclos.lua
+++ ciclos.lua
@@ -8,7 +8,12 @@ for i=2, 10, 2 do --El tercer parámetro indica como va a contar el ciclo (en es
 	print(i)
 end
 
-tabla = {"Hola", 123, false}
-for i=1, #tabla do
-	print(tabla[i])
+tabla = {"Hola", 123, false} -- en pocas palabras, un arreglo
+for i=1, #tabla do -- foreach simulado
+	print(tabla[i])--A diferencia de otros lenguajes, el primer espacio es 1 y no 0
+end
+-------------ForIpairs (foreach comun)----------------------
+tabla2 = {"hola1", "hola2", "hola3"}
+for i, v in ipairs(tabla2) do -- i = posicion del dato, v = el dato como tal, tabla1 es el arreglo o tabla que se recorre
+	print(i, v)
 end
