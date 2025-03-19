66b59f1044281bdad6e5b3c2aeb822964e334d69
Atesoide <josue.naranja@gmail.com>
2025-03-18 20:28:25 -0600

    [INTRODUCE] al ciclo pairs

--- ciclos.lua
+++ ciclos.lua
@@ -14,6 +14,19 @@ for i=1, #tabla do -- foreach simulado
 end
 -------------ForIpairs (foreach comun)----------------------
 tabla2 = {"hola1", "hola2", "hola3"}
+-- el for ipairs recorre una tabla secuencialmente, (1, 2, 3 ...) si existe un salto entre una posición 
+-- tabla[2] y consigue tabla[4] y en tabla[3] no hay nada, entonces se detendrá en tabla[2]
 for i, v in ipairs(tabla2) do -- i = posicion del dato, v = el dato como tal, tabla1 es el arreglo o tabla que se recorre
 	print(i, v)
+end
+-----------For pairs--------------------
+tabla3 = {"123", 1, 2}
+tabla3["a"] = "Dato A" --agrega un dato en la posicón ["a"] de la tabla (???)
+tabla3["b"] = "Dato B"
+tabla3[14] = "Dato 14"
+--[[El ciclo pairs recorre absolutamente toda la tabla ignorando espacios vacíos saltandose directamente hasta donde esté
+el siguiente dato, en caso de que haya posiciones que no son numericas como tabla3["a"] entonces los recorrerá en el orden
+que se le de su regalada gana]]
+for k, v in pairs(tabla3) do --misma sintaxis que el ipairs
+	print(k, v)
 end
